ee309cb841b7923a020fbcb75f2b9e39a89522c8
Bishal Neupane <85444310+earthPerson-001@users.noreply.github.com>
2024-05-31 12:46:47 +0545

    Make predictions nullable for serialization and add id field for serialization.

--- inference_api/serializers.py
+++ inference_api/serializers.py
@@ -10,9 +10,9 @@ class XRayPredictionSerializer(serializers.ModelSerializer):
 
 class XRayImageSerializer(serializers.ModelSerializer):
     predictions = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=XRayPrediction.objects.all()
+        many=True, queryset=XRayPrediction.objects.all(), allow_null=True, required=False
     )
 
     class Meta:
         model = XRayImage
-        fields = ["name", "img_file", "upload_date", "predictions"]
+        fields = ["id", "name", "img_file", "upload_date", "predictions"]
